4c314cec822e83ca2ef5966f23ad424d6c928019
Maximilian Van Hout <36654636+NerveCoordinator@users.noreply.github.com>
2019-07-03 23:21:55 -0700

    cleaned up move_atomic.sh

--- atomic/move_atmoic.sh
+++ atomic/move_atmoic.sh
@@ -1,26 +1,18 @@
 # spits atomic dataset into 10000 folders so it is more managable
 
-
-# set a variable to 0 so we can increment it
+# folder counter
 i=0
-# glob for the files starting with b
 
-echo $i
-
+# make 10000 folders
 while [ $i -le 10000 ]
 do
     mkdir sorted_structures/$i
     i=$(( $i + 1 ))    
 done
 
-#exit
-i=0
+# glob for all the files in structures
 for b in structures/*; do 
-   
-     FOLDER=$((RANDOM % 10000)) 
+     # pick a random folder  
+     FOLDER=$((RANDOM % 10000))      
      cp -v -- "$b" sorted_structures/$FOLDER
-
-     #mkdir fake_dir/F_${FOLDER}
-     #echo $FOLDER
-
 done
